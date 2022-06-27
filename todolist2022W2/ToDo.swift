3ac26b699bc3ee69d04e9a2b70964aaea16fd8db
Taiwo Oyeleye <taiwooyeleye1@gmail.com>
2022-06-27 16:47:52 -0500

    finish literation 0

---
+++ todolist2022W2/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  todolist2022W2
+//
+//  Created by Scholar on 6/27/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = "THIS IS BAD DON'T WANT THIS"
+    var important = false
+}
